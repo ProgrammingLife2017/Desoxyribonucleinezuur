cfde5366d51c0cdef1a8ddcac772402ef0c46235
Toine Hartman <tjbhartman@gmail.com>
2017-05-04 18:39:20 +0200

    Report elapsed time

--- src/main/java/programminglife/ProgrammingLife.java
+++ src/main/java/programminglife/ProgrammingLife.java
@@ -3,6 +3,9 @@ package programminglife;
 import programminglife.model.Graph;
 
 import java.io.FileNotFoundException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.TimeZone;
 
 /**
  * Created by marti_000 on 25-4-2017.
@@ -15,19 +18,24 @@ public final class ProgrammingLife {
     private static final String TB_DATA = DATA_FOLDER + "real/TB10.gfa";
     private static final String HUMAN_DATA = DATA_FOLDER + "real/chr19.hg38.w115.gfa";
 
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("m:ss.SSS", Locale.getDefault());
+
     private ProgrammingLife() {
     }
 
     public static void main(String[] args) {
         Graph g;
         String graphFile = TEST_DATA;
+        DATE_FORMAT.setTimeZone(TimeZone.getTimeZone("UTC"));
 
         if (args.length > 0) {
             graphFile = args[0];
         }
 
         try {
-            g = Graph.parse(graphFile, true);
+            long startTime = System.nanoTime();
+            g = Graph.parse(graphFile);
+            System.out.println(String.format("Parsing took %s", DATE_FORMAT.format((System.nanoTime() - startTime) / 1000000)));
         } catch (FileNotFoundException e) {
             System.err.println(String.format("File not found (%s)", graphFile));
         }
