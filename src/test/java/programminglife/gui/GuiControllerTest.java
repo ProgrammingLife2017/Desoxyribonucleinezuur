ec3ded82369292339a11cfcf43b5458987ea47dd
Yannick <yanhave@gmail.com>
2017-05-17 11:11:33 +0200

    Put a click on 2 lines higher in the hopes travis will now build

--- src/test/java/programminglife/gui/GuiControllerTest.java
+++ src/test/java/programminglife/gui/GuiControllerTest.java
@@ -73,9 +73,9 @@ public class GuiControllerTest extends FxRobot {
 
         clickOn("#txtMaxDrawDepth").type(KeyCode.DIGIT9);
         clickOn("#txtCenterNode").type(KeyCode.DIGIT1);
+        clickOn("#btnDraw");
         assertEquals("1", ((TextField) lookup("#txtCenterNode").queryFirst()).getText());
         assertEquals("9", ((TextField) lookup("#txtMaxDrawDepth").queryFirst()).getText());
-        clickOn("#btnDraw");
         assertEquals(2 * 8 + 9, ((Group) lookup("#grpDrawArea").queryFirst()).getChildren().size());
         clickOn("#btnZoomIn");
         clickOn("#btnZoomOut");
