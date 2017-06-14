ce101828336530aab1bcafda412fc4999714c11b
Ivo Wilms <I.R.G.wilms@student.tudelft.nl>
2017-06-15 01:40:42 +0200

    Wrote parser for single annotations Feature and Annotation now use file in .equals and .hashcode Add new constructor with message and cause to UnknownTypeException Create constructor and some methods for Annotation

--- src/main/java/programminglife/model/Annotation.java
+++ src/main/java/programminglife/model/Annotation.java
@@ -1,6 +1,8 @@
 package programminglife.model;
 
-import java.util.Set;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
 
 /**
  * An Annotation for a genome.
@@ -9,51 +11,90 @@ public class Annotation {
     private final String id;
     private final String file;
     private int genomeIndex;
+    private final String originalGenomeID;
 
     private final int startCoordinate;
     private final int endCoordinate;
 
-    private final String source;
-    private final String type;
-    private final String score;
-    private final String strand;
-    private final int phase; // phase can be 0, 1 or 2.
+    private final Map<String, Set<String>> textFields;
 
-    private final String originalGenomeID;
-    private final String name;
-    private final Set<String> aliases;
-    private final String displayName;
+    /**
+     * A constructor for an Annotation.
+     * @param id The ID of this Annotation.
+     * @param file The file for this Annotation.
+     * @param originalGenomeID The original String for the genome. These should be mapped to the
+     *                         genome indices in the {@link GenomeGraph}.
+     * @param startCoordinate The starting coordinate within the genome.
+     * @param endCoordinate The end coordinate within the genome.
+     */
+    public Annotation(String id, String file, String originalGenomeID,
+                      int startCoordinate, int endCoordinate) {
+        this.id = id;
+        this.file = file;
+        this.genomeIndex = -1;
+        this.originalGenomeID = originalGenomeID;
+        this.startCoordinate = startCoordinate;
+        this.endCoordinate = endCoordinate;
+        this.textFields = new LinkedHashMap<>();
+    }
 
-    private final Set<String> parents;
-    private final Set<String> children;
+    /**
+     * Add a text attribute.
+     * If the attribute already exists, this value is added to it.
+     * @param name The name of the attribute.
+     * @param value The value of the attribute.
+     */
+    public void addAttribute(String name, String value) {
+        getOrCreateAttribute(name).add(value);
+    }
 
-    private final String TargetID;
-    private final int TargetStart;
-    private final int TargetEnd;
-    private final String targetStrand;
+    /**
+     * Add multiple values to a single attribute.
+     * If the attribute did not exist yet, it is created.
+     * @param name The name of the attribute.
+     * @param values The values to add.
+     */
+    public void addMultiAttribute(String name, Set<String> values) {
+        getOrCreateAttribute(name).addAll(values);
+    }
 
-    private final String gap;
-    private final String DerivesFrom;
+    /**
+     * Add multiple values to a single attribute.
+     * If the attribute did not exist yet, it is created.
+     * @param name The name of the attribute.
+     * @param values The values to add.
+     */
+    public void addMultiAttribute(String name, String[] values) {
+        Collections.addAll(getOrCreateAttribute(name), values);
+    }
 
-    private final Set<String> notes;
-    private final Set<String> Dbxrefs;
-    private final Set<String> OntologyTerms;
-
-    private final boolean is_circular;
-
+    /**
+     * Get or create an attribute. This returns the value set for an attribute.
+     * If the attribute didn't exist yet, it is created.
+     * @param name The name of the attribute.
+     * @return The set of values for this attribute.
+     */
+    @NotNull
+    private Set<String> getOrCreateAttribute(String name) {
+        return textFields.computeIfAbsent(name, k -> new HashSet<>());
+    }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         Annotation that = (Annotation) o;
 
-        return id.equals(that.id);
+        return id.equals(that.id) && file.equals(that.file);
     }
 
     @Override
     public int hashCode() {
-        return id.hashCode();
+        return id.hashCode() + 31 * file.hashCode();
     }
 }
--- src/main/java/programminglife/parser/AnnotationParser.java
+++ src/main/java/programminglife/parser/AnnotationParser.java
@@ -1,18 +1,22 @@
 package programminglife.parser;
 
+import org.jetbrains.annotations.NotNull;
 import programminglife.model.Annotation;
 import programminglife.model.exception.UnknownTypeException;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.util.EnumSet;
 
 /**
- * A Parser for annotations.
+ * A Parser for {@link Annotation Annotations}.
  */
 public class AnnotationParser implements Runnable {
     private File file;
 
+    /**
+     * Constructor for an AnnotationParser.
+     * @param file The {@link File} to parse.
+     */
     public AnnotationParser(File file) {
         this.file = file;
     }
@@ -22,6 +26,9 @@ public class AnnotationParser implements Runnable {
         parseFile();
     }
 
+    /**
+     * Parse the file.
+     */
     private void parseFile() {
         // TODO: implement
 
@@ -32,9 +39,9 @@ public class AnnotationParser implements Runnable {
     }
 
     /**
-     * Parse a String as an Annotation
+     * Parse a String as an Annotation.
      * @param line The String to parse
-     * @return The parsed Annotation. If this line is empty or a comment (starts with #), return null.
+     * @return The parsed Annotation. If this line is empty or a comment (starts with #), this returns null.
      * @throws UnknownTypeException If the line is malformed.
      */
     Annotation parseLine(String line) throws UnknownTypeException {
@@ -47,17 +54,72 @@ public class AnnotationParser implements Runnable {
 
         if (columns.length != 9) {
             throw new UnknownTypeException(String.format(
-                    "The annotation does not contain the required 9 columns. Actua number of columns: %d",
+                    "The annotation does not contain the required 9 columns. Actual number of columns: %d",
                     columns.length
             ));
         }
 
+        String genomeID = decode(columns[0], Encoding.BASIC);
 
+        int start;
+        int end;
+        String id;
 
-        // Notes:
-        //  - ignore empty lines and lines starting with # (comments)
-        //  - use decode to decode values
-        throw new Error("Not yet implemented");
+        try {
+            start = Integer.parseInt(columns[3]);
+            end   = Integer.parseInt(columns[4]);
+
+            String afterID = columns[8].substring(columns[8].indexOf("ID="));
+            int index = afterID.indexOf(';');
+            if (index == -1) {
+                index = afterID.length();
+            }
+            id = afterID.substring(0, index);
+        } catch (NumberFormatException | IndexOutOfBoundsException e) {
+            throw new UnknownTypeException(
+                    "One of the columns of this Annotation could not be parsed.",
+                    e
+            );
+        }
+
+        Annotation annotation = new Annotation(id, file.getAbsolutePath(), genomeID, start, end);
+
+        annotation.addAttribute("source", decode(columns[1], Encoding.BASIC));
+        annotation.addAttribute("type", decode(columns[2], Encoding.BASIC));
+        annotation.addAttribute("score", decode(columns[5], Encoding.BASIC));
+        annotation.addAttribute("strand", decode(columns[6], Encoding.BASIC));
+        annotation.addAttribute("phase", decode(columns[7], Encoding.BASIC));
+
+        parseAttributes(annotation, columns[8]);
+
+        return annotation;
+    }
+
+    /**
+     * Parse a String containing attributes for an {@link Annotation}.
+     * @param annotation The annotation to add the attributes to.
+     * @param attributes The String containing the attributes. This String should be column 9 in the gff spec.
+     * @throws UnknownTypeException If the attributes are malformed.
+     */
+    private void parseAttributes(Annotation annotation, String attributes) throws UnknownTypeException {
+        String[] attributesArray = attributes.split(";");
+        for (String attribute : attributesArray) {
+            String[] values = attribute.split(",");
+            String name;
+            try {
+                name = values[0].substring(0, values[0].indexOf('='));
+                values[0] = values[0].substring(values[0].indexOf('=') + 1);
+            } catch (IndexOutOfBoundsException e) {
+                throw new UnknownTypeException(
+                        String.format("Malformed key-value pair in attributes: \"%s\".", attribute),
+                        e
+                );
+            }
+            for (int i = 0; i < values.length; i++) {
+                values[i] = decode(values[i], Encoding.EXTENDED);
+            }
+            annotation.addMultiAttribute(name, values);
+        }
     }
 
     /**
@@ -66,54 +128,70 @@ public class AnnotationParser implements Runnable {
      * @param encoding The encoding that is decoded (this should be the encoding used to encode the string).
      * @return The decoded String.
      */
+    @NotNull
     private String decode(String encoded, Encoding encoding) {
         String result = encoded;
 
         if (encoding.contains(EncodingSubtype.WHITESPACE)) {
-            result  .replaceAll("%09", "\t")
+            result = result.replaceAll("%09", "\t")
                     .replaceAll("%0A", "\n")
                     .replaceAll("%0D", "\r");
         }
 
         if (encoding.contains(EncodingSubtype.KEY_VALUE)) {
-            result  .replaceAll("%3B", ";")
+            result = result.replaceAll("%3B", ";")
                     .replaceAll("%3D", "=")
                     .replaceAll("%26", "&")
                     .replaceAll("%2C", ",");
         }
 
         if (encoding.contains(EncodingSubtype.SPACE)) {
-            result.replaceAll("%20", " ");
+            result = result.replaceAll("%20", " ");
         }
 
         if (encoding.contains(EncodingSubtype.PERCENT)) {
-            result.replaceAll("%25", "%");
+            result = result.replaceAll("%25", "%");
         }
 
         return result;
     }
 
+    /**
+     * An Encoding, which is defined by a set of {@link EncodingSubtype EncodingSubtypes}.
+     */
     private enum Encoding {
         NONE(EnumSet.noneOf(EncodingSubtype.class)),
         BASIC(EnumSet.of(EncodingSubtype.WHITESPACE, EncodingSubtype.PERCENT)),
         EXTENDED(EnumSet.of(
                 EncodingSubtype.WHITESPACE,
                 EncodingSubtype.PERCENT,
-                EncodingSubtype.SPACE,
                 EncodingSubtype.KEY_VALUE
         ));
 
         private final EnumSet<EncodingSubtype> encodingSubtypes;
 
-        private Encoding(EnumSet<EncodingSubtype> encodingSubTypes) {
+        /**
+         * Constructor for an Encoding.
+         * @param encodingSubTypes An {@link EnumSet} of {@link EncodingSubtype EncodingSubtypes}
+         *                         that this Encoding uses.
+         */
+        Encoding(EnumSet<EncodingSubtype> encodingSubTypes) {
             this.encodingSubtypes = encodingSubTypes;
         }
 
+        /**
+         * Whether this Encoding contains a certain {@link EncodingSubtype}.
+         * @param subtype The Subtype to check for.
+         * @return Whether this Encoding contains the subtype.
+         */
         private boolean contains(EncodingSubtype subtype) {
             return encodingSubtypes.contains(subtype);
         }
     }
 
+    /**
+     * A subtype of an {@link Encoding}. This is a name for a set of encodings for some characters.
+     */
     private enum EncodingSubtype {
         PERCENT,
         SPACE,
