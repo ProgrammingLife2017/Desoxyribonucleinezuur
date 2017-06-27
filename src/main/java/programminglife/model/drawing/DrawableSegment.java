ffb53810c414b5b733f26b0045a613df36284d93
Iwan Hoogenboom <iwanhwow@gmail.com>
2017-06-27 18:05:35 +0200

    Inital new node sizes. Opinions?

--- src/main/java/programminglife/model/drawing/DrawableSegment.java
+++ src/main/java/programminglife/model/drawing/DrawableSegment.java
@@ -9,6 +9,8 @@ import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static java.lang.Integer.min;
+
 /**
  * A Segment that also Implements {@link Drawable}.
  */
@@ -109,7 +111,7 @@ public class DrawableSegment extends DrawableNode {
         int segmentLength = this.getSequenceLength();
         double width, height;
 
-        width = 10 + Math.pow(segmentLength, 1.0 / 2);
+        width = min(10 * segmentLength, 100) + Math.pow(segmentLength * 100, 1.0 / 2);
         height = NODE_HEIGHT;
 
         this.setSize(width * zoomLevel, height * zoomLevel);
